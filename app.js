04ea44428bf77d639c176801615825e7fc16385b
Kashish Behera <kashishbehera9@gmail.com>
2026-06-16 17:58:33 +0530

    JavaScript added into the project

---
+++ app.js
@@ -0,0 +1,22 @@
+const addTodoBtn = document.getElementById("addTodoBtn");
+
+const inputTag = document.getElementById("todoInput")
+let todoText; //This should be populated when the user clicks on Add Button
+
+let todos = [];
+
+
+addTodoBtn.addEventListener("click", () => {
+    console.log("Hey I just clicked")
+    todoText = inputTag.value
+    console.log(todoText)
+    inputTag.value = ""
+
+    let todo = {
+        title: todoText,
+        isCompleted: false,
+    }
+
+    todos.push(todo)
+    localStorage.setItem("todos", JSON.stringify(todos))
+})
