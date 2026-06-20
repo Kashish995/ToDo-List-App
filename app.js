c52b60accd7cd6fb709587a0e26b97dbfd986d1a
Kashish Behera <kashishbehera9@gmail.com>
2026-06-20 19:19:37 +0530

    Upadated the javaScript Logic

--- app.js
+++ app.js
@@ -1,70 +1,87 @@
-const addTodoBtn = document.getElementById("addTodoBtn");
-const inputTag = document.getElementById("todoInput");
-const todoListUl = document.getElementById("todoList");
+const addTodoBtn = document.getElementById("addTodoBtn")
+const inputTag = document.getElementById("todoInput")
+const todoListUl = document.getElementById("todoList")
 
-let todoText; //This should be populated when the user clicks on Add Button
+let todoText; // This should be populated when the user clicks on Add button
 let todos = [];
-let todosString = localStorage.getItem("todos");
-//If we have todos int he localStorage, we will read it
-if (todosString) {
-  todos = JSON.parse(todosString);
+let todosString = localStorage.getItem("todos")
+// If we have todos in the localStorage, we will read it
+if(todosString){
+    todos = JSON.parse(todosString);
 }
 
 
 
-const populateTodos = () => {
-  let string = "";
-
-  for (const todo of todos) {
-    string += ` <li id="todo-${todo.id}" class="todo-item ${todo.isCompleted? "completed":""}" >
-                    <input type="checkbox" class="todo-checkbox" ${todo.isCompleted? "checked":""} >
-                    <span class="todo-text">${todo.title}</span>
-                    <button class="delete-btn">×</button>
-                </li>`
-  }
-  todoListUl.innerHTML =  string;
+const populateTodos = ()=>{ 
+    let string = "";
+    for (const todo of todos) {
+        string += `<li id="todo-${todo.id}" class="todo-item ${todo.isCompleted? "completed":""}">
+            <input type="checkbox" class="todo-checkbox" ${todo.isCompleted? "checked":""} >
+            <span class="todo-text">${todo.title}</span>
+            <button class="delete-btn">×</button>
+        </li>`
+    }
+    todoListUl.innerHTML =  string
+    
 }
 
-addTodoBtn.addEventListener("click", () => {
-  todoText = inputTag.value;
-  inputTag.value = "";
 
-  let todo = {
-    id: todos.length,
-    title: todoText,
-    isCompleted: false
-  };
-
-  todos.push(todo);
-  localStorage.setItem("todos", JSON.stringify(todos))
-  populateTodos()
+addTodoBtn.addEventListener("click", ()=>{ 
+    todoText = inputTag.value  
+    inputTag.value = ""
+    let todo = {
+        id: todos.length,
+        title: todoText,
+        isCompleted: false
+    }
+    todos.push(todo)
+    localStorage.setItem("todos", JSON.stringify(todos))
+    populateTodos()
 })
 
 populateTodos()
 
 const todoCheckboxes = document.querySelectorAll(".todo-checkbox")
 
-todoCheckboxes.forEach((element) => {
-  console.log(element)
-  element.addEventListener("click", (e) => {
-    if(e.target.checked) {
-      element.parentNode.classList.add("completed")
-      console.log(element.parentNode.id)
-      // Grab this todo from todos array and update the todos array to set this todos isCompleted attribute as true
-      todos  = todos.map(todo => {
-        console.log("todo-" + todo.id, element.parentNode.id)
-        if("todo-" + todo.id == element.parentNode.id){
-          return {...todo, isCompleted: true}
+todoCheckboxes.forEach((element)=>{  
+    element.addEventListener("click", (e)=>{ 
+        if(e.target.checked){
+            element.parentNode.classList.add("completed") 
+            // Grab this todo from todos array and update the todos array to set this todo's isCompleted attribute as true
+            todos = todos.map(todo =>{ 
+                if("todo-" + todo.id == element.parentNode.id){
+                    return {...todo, isCompleted: true}
+                }
+                else{ 
+                    return todo
+                }
+            }) 
+            localStorage.setItem("todos", JSON.stringify(todos))
         }
         else{
-          console.log(todo)
-          return todo
+            element.parentNode.classList.remove("completed")
+             // Grab this todo from todos array and update the todos array to set this todos isCompleted attribute as false
+            todos = todos.map(todo =>{ 
+                if("todo-" + todo.id == element.parentNode.id){
+                    return {...todo, isCompleted: false}
+                }
+                else{ 
+                    return todo
+                }
+            }) 
+            localStorage.setItem("todos", JSON.stringify(todos))
         }
-      })
-      console.log(todos)
-      localStorage.setItem("todos", JSON.stringify(todos))
-    }else{
-       element.parentNode.classList.remove("completed")
-    }
-  })
+    })
+})
+
+let deleteBtns = document.querySelectorAll(".delete-btn")
+console.log(deleteBtns)
+deleteBtns.forEach((element) => {
+    element.addEventListener("click", (e) => {
+        console.log(e.target.parentNode.id)
+        let todos = todos.filter((todo) => {
+            return ("todo-" + todo.id) !== (e.target.parentNode.id)
+        })
+        console.log(todos)
+    })
 })
